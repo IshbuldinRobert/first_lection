6a49f36c20095f047fa1652642f89cc253e6bf8a
Robert <ishbuldinrobert2002@gmail.com>
2023-04-26 18:58:07 +0500

    add condition if-else

---
+++ example_5/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+
+if(username.ToLower() == "мурад")
+{
+    Console.Write("Привет, никто) а ой, сорян, Мурад!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
